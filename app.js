de661d93a627616c1e90b9d342c57c2e8ff347ec
Nicholas Dombrosky <ndombrosky@gmail.com>
2021-02-10 10:51:32 -0700

    added user functions and made sure we can connect to database

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require('./db');
 console.log(` app name = ${process.env.APP_NAME}`);
 console.log(`title = ${process.env.title}`);
 
 app.use(express.urlencoded({extended: false}))
 
 //api for the client (browser)
-app.get('/users', (req, res) =>{
-        res.send('all users')
-});
-app.post('/createUser', (req, res) =>{
-        res.send('new user created');
-})
+app.get('/users', db.getUsers);
+app.post('/createUser', (db.createUser))
 
 
 app.listen(port, () =>{
--- db.js
+++ db.js
@@ -1,2 +1,72 @@
 //api to the database
+const Pool = require("pg-pool");
+const url = require("url");
+const dbConnectionString = process.env.DATABASE_URL;
 
+const params = url.parse(dbConnectionString);
+
+const auth = params.auth.split(":");
+let SSL = process.env.SSL || { rejectUnauthorized: false };
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  ssl: SSL,
+};
+const pool = new Pool(config);
+
+const getUsers = (req, res) => {
+  let getUsersSQL = "select * from users";
+  pool.query(getUsersSQL, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+    res.status(200).json(results.rows);
+  });
+};
+const createUser = (req, res) => {
+  const email = req.body.email;
+  const name = req.body.name;
+  let createUserSQL = "insert into users (email, name) values ($1, $2)";
+  pool.query(createUserSQL, [email, name], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+    res.status(200).json(results);
+  });
+};
+const deleteUser = (req, res) => {
+  const name = req.body.name;
+  let deleteUserSQL = "delete from users where name = $1";
+  pool.query(deleteUserSQL, [name], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+    res.status(200).json(results);
+  });
+};
+const updateUser = (req, res) => {
+  const name = req.body.name;
+  const id = req.body.id;
+  let updateUserSQL = "update user set name = $1 where id = $1";
+  pool.query(updateUserSQL, [name, id], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+    res.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+  deleteUser,
+  updateUser,
+};
